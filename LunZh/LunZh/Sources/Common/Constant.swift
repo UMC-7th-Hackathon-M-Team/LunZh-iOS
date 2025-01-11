23cdb55d30df59d0c45ca186fd359a98f38a0be4
이예성 <yeseong_lee@naver.com>
2025-01-11 22:07:02 +0900

    폰트 Constant 설정

--- LunZh/LunZh/Sources/Common/Constant.swift
+++ LunZh/LunZh/Sources/Common/Constant.swift
@@ -20,8 +20,33 @@ struct Constants {
         static let gray400 = UIColor(named: "gray400")
         static let gray500 = UIColor(named: "gray500")
         static let gray600 = UIColor(named: "gray600")
-        static let gray700 = UIColor(named: "Gray700")
+        static let gray700 = UIColor(named: "gray700")
         static let gray800 = UIColor(named: "gray800")
     }
     
+    static let TasteList = [
+        "비빔밥",
+        "김치찌개",
+        "된장찌개",
+        "삼겹살",
+        "불고기",
+        "잡채",
+        "떡볶이",
+        "라면",
+        "냉면",
+        "칼국수",
+        "김밥",
+        "순대국밥",
+        "부대찌개",
+        "치킨",
+        "해물파전",
+        "소고기미역국",
+        "제육볶음",
+        "닭갈비",
+        "갈비탕",
+        "해장국"
+    ]
+    
+    static let superViewHeight = UIScreen.main.bounds.height
+    static let superViewWidth = UIScreen.main.bounds.width
 }
