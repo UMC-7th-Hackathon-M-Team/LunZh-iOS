8747e36f08953e37086dca3f32b0f59fa4804a0d
이예성 <yeseong_lee@naver.com>
2025-01-11 18:17:28 +0900

    컬러 에셋 설정

--- LunZh/LunZh/Sources/Common/Constant.swift
+++ LunZh/LunZh/Sources/Common/Constant.swift
@@ -1,8 +1,27 @@
-//
-//  Constant.swift
-//  LunZh
-//
-//  Created by 이현주 on 1/11/25.
-//
+import UIKit
 
-import Foundation
+struct Constants {
+    
+    struct Colors {
+        static let red20 = UIColor(named: "red20")
+        static let red40 = UIColor(named: "red40")
+        static let red60 = UIColor(named: "red60")
+        static let red80 = UIColor(named: "red80")
+        static let red100 = UIColor(named: "red100")
+        static let white = UIColor(named: "white")
+        static let yellow20 = UIColor(named: "yellow20")
+        static let yellow40 = UIColor(named: "yellow40")
+        static let yellow60 = UIColor(named: "yellow60")
+        static let yellow80 = UIColor(named: "yellow80")
+        static let yellow100 = UIColor(named: "yellow100")
+        static let gray100 = UIColor(named: "gray100")
+        static let gray200 = UIColor(named: "gray200")
+        static let gray300 = UIColor(named: "gray300")
+        static let gray400 = UIColor(named: "gray400")
+        static let gray500 = UIColor(named: "gray500")
+        static let gray600 = UIColor(named: "gray600")
+        static let gray700 = UIColor(named: "Gray700")
+        static let gray800 = UIColor(named: "gray800")
+    }
+    
+}
